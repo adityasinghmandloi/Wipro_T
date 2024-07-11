be37b68385e173186bbdec907bb441be411f497e
Aditya Singh Mandloi <108330859+adityasinghmandloi@users.noreply.github.com>
2024-07-11 18:15:12 +0530

    Update Helthrecords.java

--- Helthrecords.java
+++ Helthrecords.java
@@ -4,8 +4,8 @@ interface HealthRecord {
 }
 
 class Person {
-    private String name;
-    private int id;
+    String name;
+    int id;
 
     public Person(String name, int id) {
         this.name = name;
